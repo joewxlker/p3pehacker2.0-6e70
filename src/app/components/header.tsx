1ce6c0f3f740e43a59f6ad2c99f2dab626c39cc9
Joseph Walker <99849204+joewxlker@users.noreply.github.com>
2023-11-26 00:13:45 +0800

    fix builds

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Button } from "./button";
 import { type ProjectInfo } from "~/app/models/project.model";
-import { Socials } from "~/app/components/socials.tsx"
+import { Socials } from "~/app/components/socials"
 
 export const Header: FC<{ layout: string; config: ProjectInfo }> = ({
   layout,
